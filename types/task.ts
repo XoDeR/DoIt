35004fe7b373f7781ebf3d448a044b0e67a9df4a
Volodymyr <XoDeR@users.noreply.github.com>
2023-10-08 18:04:59 +0200

    added task types (with zod)

--- types/task.ts
+++ types/task.ts
@@ -1,5 +1,19 @@
 import { z } from "zod";
 
+export const taskInputSchema = z.object({
+  title: z.string(),
+  content: z.string().optional(),
+  userId: z.string(),
+});
+
+export const updateTaskInputSchema = z.object({
+  title: z.string().optional(),
+  content: z.string().optional(),
+});
+
+export type UpdateTaskInput = z.infer<typeof updateTaskInputSchema>;
+export type TaskInput = z.infer<typeof taskInputSchema>;
+
 export const taskSchema = z.object({
   id: z.string(),
   title: z.string(),
@@ -8,3 +22,11 @@ export const taskSchema = z.object({
 });
 
 export type Task = z.infer<typeof taskSchema>;
+
+export const taskPreviewSchema = z.object({
+  id: z.string(),
+  title: z.string(),
+  userId: z.string(),
+});
+
+export type TaskPreview = z.infer<typeof taskPreviewSchema>;
